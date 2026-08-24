df9f0d23dc73f0be9bfdc3835a1b439418df893e
kimhoho8601-alt <kimhoho8601@gmail.com>
2026-08-24 15:44:50 +0900

    Add Supabase config

---
+++ config.js
@@ -0,0 +1,6 @@
+window.TREE_CONFIG = {
+  supabaseUrl: 'https://tedbkobhltarqibjhfhk.supabase.co',
+  supabaseKey: 'sb_publishable_CS5rpk5S1eYBFxyUzCQ63w_NzPpwOZ2',
+  capacity: 200,
+  pollMs: 1800
+};
